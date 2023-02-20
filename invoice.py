458ae2e58d769d87ce90a5607001c78452576724
Raimon Esteve <raimonesteve@gmail.com>
2023-02-20 08:35:13 +0100

    AttributeError: 'NoneType' object has no attribute 'set_maturities' #051462

--- invoice.py
+++ invoice.py
@@ -409,6 +409,8 @@ class ModifyMaturities(Wizard):
 
         config = Configuration(1)
         invoice = self.ask.invoice
+        if not invoice:
+            return 'next_'
 
         if self.ask.pending_amount:
             # TODO remove raise user error
