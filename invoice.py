99a8dddae36bad745076aa01c23f91d10c7e6f44
Bernat Brunet <bernat@nan-tic.com>
2023-05-31 09:59:32 +0200

    Move the RescheduleLines inherit wizar to the move files.
    Add the description field to the terms lines.
    
    Save the move and move line description in the new move and move lines
    created.
    
    Show the actual payment lines with the amount, amturity date and
    description in the first wizard view to have the base information to
    update.

--- invoice.py
+++ invoice.py
@@ -41,66 +41,7 @@ class Invoice(metaclass=PoolMeta):
         cls.post(invoices)
         invoice_types = set([i.type for i in invoices])
 
-        if (config.maturities_on_customer_post and 'out' in invoice_types):
+        if ((config.maturities_on_customer_post and 'out' in invoice_types)
+                or (config.maturities_on_supplier_post
+                    and 'in' in invoice_types)):
             return cls.reschedule_lines_to_pay(invoices)
-        if (config.maturities_on_supplier_post and 'in' in invoice_types):
-            return cls.reschedule_lines_to_pay(invoices)
-
-    def set_maturities(self, maturity_dates):
-        pool = Pool()
-        Move = pool.get('account.move')
-        Line = pool.get('account.move.line')
-
-        if not self.move:
-            return
-
-        to_create, to_write, to_delete = [], [], []
-
-        Move.draft([self.move])
-
-        processed = set()
-        for maturity in maturity_dates:
-            amount = maturity.amount
-            if self.type == 'out':
-                amount = amount.copy_negate()
-            new_line = self._get_move_line(maturity.date, amount)
-            # With the speedup patch this may return a Line instance
-            # XXX: Use instance when patch is commited
-            if isinstance(new_line, Line):
-                new_line = new_line._save_values
-            line = maturity.move_line
-            if not line:
-                new_line['move'] = self.move.id
-                to_create.append(new_line)
-                continue
-            values = {}
-            for field, value in new_line.items():
-                current_value = getattr(line, field)
-                if isinstance(current_value, Model):
-                    current_value = current_value.id
-                if current_value != value:
-                    values[field] = value
-            processed.add(line)
-            if values:
-                quantize = Decimal(10) ** -Decimal(maturity.currency.digits)
-                if 'credit' in values:
-                    values['credit'] = Decimal(values.get('credit')).quantize(
-                            quantize)
-                if 'debit' in values:
-                    values['debit'] = Decimal(values.get('debit')).quantize(
-                            quantize)
-                to_write.extend(([line], values))
-
-        for line in self.move.lines:
-            if line.account == self.account:
-                if line not in processed:
-                    to_delete.append(line)
-
-        if to_create:
-            Line.create(to_create)
-        if to_write:
-            Line.write(*to_write)
-        if to_delete:
-            Line.delete(to_delete)
-
-        Move.post([self.move])
