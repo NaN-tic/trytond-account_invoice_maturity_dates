82dfc12d9fc46365b719e0088f63615a907f437f
Albert Cervera i Areny <albert@nan-tic.com>
2024-02-19 16:19:33 +0100

    Pyflakes fixes

--- configuration.py
+++ configuration.py
@@ -1,9 +1,7 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
-from trytond.model import fields, ModelSQL
+from trytond.model import fields
 from trytond.pool import PoolMeta, Pool
-from trytond.modules.company.model import CompanyValueMixin
-from trytond.pyson import Eval
 
 maturities_on_customer_post = fields.Boolean('Show Maturities on '
     'Customer Invoices Post')
